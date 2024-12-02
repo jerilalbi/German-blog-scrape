a09165a8aa23609d07337fe2d2d7c9e76f206e8c
Jeril Albi <devjerilalbi@gmail.com>
2024-12-02 18:31:17 +0530

    new website to scrape data

--- scape_data.js
+++ scape_data.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const csvWriter = require('csv-writer');
 const axios = require('axios');
 const { parseStringPromise } = require('xml2js');
-const { timeout } = require('puppeteer');
 const csv = require('csv-parser');
-const whois = require('whois-json');
+const url = require('url');
+const { timeout } = require('puppeteer');
 
 const linuxUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36";
 
@@ -166,23 +166,24 @@ function makeExcelFile(websiteData) {
     // const fileExists = fs.existsSync(filePath);
 
     const writer = csvWriter.createObjectCsvWriter({
-        path: 'website_data_sample.csv',
+        path: 'website_data2.csv',
         header:
             [
                 { id: 'name', title: 'Name' },
                 { id: 'monthly_viewers', title: 'Viewers' },
-                { id: 'last_updated', title: 'Last Updated' },
+                // { id: 'last_updated', title: 'Last Updated' },
                 { id: 'url', title: 'URL' },
-                { id: 'email', title: 'Email' },
-            ]
+                // { id: 'email', title: 'Email' },
+            ],
+        append: true
     });
 
-    const formattedData = websiteData.map(item => ({
-        ...item,
-        url: `=HYPERLINK("${item.url}", "${item.url}")`
-    }));
+    // const formattedData = websiteData.map(item => ({
+    //     ...item,
+    //     url: `=HYPERLINK("${item.url}", "${item.url}")`
+    // }));
 
-    writer.writeRecords(formattedData)
+    writer.writeRecords(websiteData)
         .then(() => {
             console.log('CSV file written successfully');
         })
@@ -257,7 +258,7 @@ async function checkXmlPage(link) {
     } catch (error) {
         console.log(error)
     }
-})()
+})
 
 function readCsvfile(path) {
     return new Promise((resolve, reject) => {
@@ -268,3 +269,162 @@ function readCsvfile(path) {
             .on('error', (error) => reject(error))
     })
 }
+
+async function getBlogDatas() {
+    try {
+        const currentDataSet = new Set();
+
+        const currentDataPath = "website_data_sample.csv";
+        const currentData = await readCsvfile(currentDataPath);
+        currentData.forEach(item => currentDataSet.add(item.Name));
+
+        const links = [
+            "https://www.bloggerei.de/rubrik_15_Computerblogs",
+            "https://www.bloggerei.de/rubrik_8_Fotoblogs",
+            "https://www.bloggerei.de/rubrik_17_Funnyblogs",
+            "https://www.bloggerei.de/rubrik_10_Hobbyblogs",
+            "https://www.bloggerei.de/rubrik_19_Jobblogs",
+            "https://www.bloggerei.de/rubrik_4_Kulturblogs",
+            "https://www.bloggerei.de/rubrik_12_Literaturblogs",
+            "https://www.bloggerei.de/rubrik_13_Musikblogs",
+            "https://www.bloggerei.de/rubrik_18_Privateblogs",
+            "https://www.bloggerei.de/rubrik_14_Seoblogs",
+            "https://www.bloggerei.de/rubrik_3_Sportblogs",
+            "https://www.bloggerei.de/rubrik_16_Tierblogs",
+            "https://www.bloggerei.de/rubrik_1_Wirtschaftsblogs",
+            "https://www.bloggerei.de/rubrik_21_Corporateblogs",
+            "https://www.bloggerei.de/rubrik_11_Freizeitblogs",
+            "https://www.bloggerei.de/rubrik_24_Gourmetblogs",
+            "https://www.bloggerei.de/rubrik_6_Internetblogs",
+            "https://www.bloggerei.de/rubrik_26_Jurablogs",
+            "https://www.bloggerei.de/rubrik_5_Kunstblogs",
+            "https://www.bloggerei.de/rubrik_20_Medizinblogs",
+            "https://www.bloggerei.de/rubrik_2_Politikblogs",
+            "https://www.bloggerei.de/rubrik_7_Reiseblogs",
+            "https://www.bloggerei.de/rubrik_25_Spieleblogs",
+            "https://www.bloggerei.de/rubrik_23_Stadtblogs",
+            "https://www.bloggerei.de/rubrik_27_Umweltblogs",
+            "https://www.bloggerei.de/rubrik_22_Wissenschaftsblogs",
+        ];
+
+        let pageLimit = 30;
+        let testIndex = 1;
+        const websiteData = [];
+
+        const browser = await puppeteer.launch({
+            headless: false,
+        });
+
+        const page = await browser.newPage();
+        await page.setUserAgent(linuxUserAgent);
+
+        for (let link of links) {
+            for (let i = 19; i <= pageLimit; i++) {
+                try {
+                    console.log(testIndex);
+                    testIndex++;
+
+                    await page.goto(`${link}_${i}`, { waitUntil: 'networkidle2' })
+
+                    await page.waitForSelector(".maincontent", { timeout: "60000" });
+
+                    pageLimit = await page.$$eval('.pagination-bottom > a ', (anchors) => parseInt(anchors[anchors.length - 1].innerText, 10));
+
+                    // const blogs = await page.evaluate(() => {
+                    //     try {
+                    //         const blogData = [];
+                    //         const blogElements = document.querySelectorAll('.bloginfos');
+
+                    //         blogElements.forEach((blog) => {
+                    //             const rawLink = blog.querySelector('.extblog').href;
+                    //             const rawDate = blog.querySelector('.posttime').innerText;
+                    //             const parsedUrl = new URL(rawLink);
+                    //             const link = parsedUrl.protocol + '//' + parsedUrl.hostname;
+                    //             const dateData = rawDate.match(/\d+/);
+                    //             const date = parseInt(dateData[0], 10);
+
+                    //             blogData.push({ link, date });
+                    //         })
+                    //         return blogData;
+                    //     } catch (error) {
+                    //         console.log(error);
+                    //     }
+                    // });
+                    const blogs = await page.$$eval(".bloginfos", el => {
+                        const blogData = [];
+                        el.forEach((blog) => {
+                            const rawLink = blog.querySelector('.extblog').href;
+                            const rawDate = blog.querySelector('.posttime').innerText;
+                            const parsedUrl = new URL(rawLink);
+                            const link = parsedUrl.protocol + '//' + parsedUrl.hostname;
+                            const dateData = rawDate.match(/\d+/);
+                            const date = parseInt(dateData[0], 10);
+
+                            blogData.push({ link, date });
+                        })
+                        return blogData;
+                    })
+                    console.log(blogs);
+
+                    if (blogs[blogs.length - 1].date >= 365) {
+                        for (const blog of blogs) {
+                            if (blog.link.toLowerCase().endsWith(".de") && blog.date > 365) {
+                                try {
+                                    console.log(blog.link)
+                                    const blogPage = await browser.newPage();
+                                    await blogPage.setUserAgent(linuxUserAgent);
+
+                                    await blogPage.goto(blog.link, { waitUntil: 'networkidle2' })
+
+                                    await blogPage.waitForSelector("body", { timeout: "60000" })
+
+                                    const elementCount = await blogPage.evaluate(() => {
+                                        return document.body.querySelectorAll('*').length;
+                                    });
+
+                                    if (elementCount > 10) {
+                                        if (!currentDataSet.has(blog.link)) {
+                                            const trafficPage = await browser.newPage();
+                                            await trafficPage.setUserAgent(linuxUserAgent);
+                                            await trafficPage.goto(`https://data.similarweb.com/api/v1/data?domain=${blog.link}`, { waitUntil: 'networkidle2' });
+
+                                            const jsonData = await trafficPage.evaluate(() => {
+                                                return JSON.parse(document.body.innerText);
+                                            });
+
+                                            const avgTrafficData = parseInt((jsonData["EstimatedMonthlyVisits"]["2024-10-01"] + jsonData["EstimatedMonthlyVisits"]["2024-09-01"] + jsonData["EstimatedMonthlyVisits"]["2024-08-01"]) / 3);
+
+                                            if (avgTrafficData >= 1000 && avgTrafficData <= 10000) {
+                                                websiteData.push({
+                                                    name: new URL(blog.link).hostname.replace(/^www\./, ''),
+                                                    monthly_viewers: avgTrafficData,
+                                                    url: blog.link,
+                                                })
+                                            }
+                                            await new Promise(r => setTimeout(r, 3000))
+                                            await trafficPage.close();
+                                        }
+                                    }
+                                    await blogPage.close();
+                                } catch (error) { }
+                            }
+                        }
+                    }
+                    if (websiteData.length > 0) {
+                        makeExcelFile(websiteData);
+                    }
+                    await new Promise(r => setTimeout(r, 2000))
+                } catch (error) {
+
+                }
+            }
+
+        }
+        makeExcelFile(websiteData);
+        // await browser.close();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getBlogDatas();
